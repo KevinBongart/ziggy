7ddb4902b13f3c2de1d5dde5948c025b6863e01e
Kevin Bongart <contact@kevinbongart.net>
2013-04-03 19:31:57 -0400

    zsh conf

---
+++ cookbooks/dotfiles/attributes/default.rb
@@ -0,0 +1,4 @@
+default[:dotfiles][:home_directory]  = "/root/"
+default[:dotfiles][:dotfiles_directory]  = "/root/dotfiles"
+default[:dotfiles][:repository] = "git://github.com/KevinBongart/dotfiles.git"
+default[:dotfiles][:branch] = "ubuntu"
